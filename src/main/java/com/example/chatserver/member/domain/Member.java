f48e0f72715ad0bf74ddf38b46cf97ce8bc03d5d
Jina Ham <hamjina61@gmail.com>
2025-03-24 14:08:45 +0900

    first commit

---
+++ src/main/java/com/example/chatserver/member/domain/Member.java
@@ -0,0 +1,30 @@
+package com.example.chatserver.member.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonTypeResolver;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default //기본값 설정을 위함
+    private Role role = Role.USER;
+}
